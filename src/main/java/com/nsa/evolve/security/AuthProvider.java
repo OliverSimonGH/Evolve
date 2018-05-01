9a7f28e9200fcaeebf49d8df37b627e3eda3086f
Oliver Simon <simonom@cardiff.ac.uk>
2018-05-01 13:37:29 +0100

    Added a logger to my code and configured it

--- src/main/java/com/nsa/evolve/security/AuthProvider.java
+++ src/main/java/com/nsa/evolve/security/AuthProvider.java
@@ -3,6 +3,8 @@ package com.nsa.evolve.security;
 import com.nsa.evolve.dto.AccountDetails;
 import com.nsa.evolve.service.AccountService;
 import com.nsa.evolve.service.impl.AccountDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -28,6 +30,9 @@ public class AuthProvider implements AuthenticationProvider {
     @Autowired
     private AccountService accountService;
 
+    private static final Logger infoLogger = LoggerFactory.getLogger("InfoLog");
+    private static final Logger warnLogger = LoggerFactory.getLogger("WarnLog");
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
@@ -35,20 +40,42 @@ public class AuthProvider implements AuthenticationProvider {
 
         AccountDetails accountDetails = (AccountDetails) accountDetailsService.loadUserByUsername(username);
 
+        System.out.println(accountDetails);
+//        If the account exists
         if (accountDetails != null){
+
+//            If password is not hashed
             if (!(accountDetails.getPassword().length() == 60 && accountDetails.getPassword().contains("$2a$10$"))){
+
+//                If password entered equals unhashed password in database
                 if (password.equalsIgnoreCase(accountDetails.getPassword())){
                     String encodedPassword = passwordEncoder.encode(password);
-                    accountService.updatePassword(accountDetails.getId(), encodedPassword);
-                    return new UsernamePasswordAuthenticationToken(accountDetails, accountDetails.getPassword(), accountDetails.getAuthorities());
-                } else throw new BadCredentialsException("Bad credentials");
-            }
-            else if (BCrypt.checkpw(password, accountDetails.getPassword())){
-                return new UsernamePasswordAuthenticationToken(accountDetails, accountDetails.getPassword(), accountDetails.getAuthorities());
-            }
-        } else throw new BadCredentialsException("Authentication failed for " + username);
 
-        throw new UsernameNotFoundException("User not found");
+                    //Update unhashed password to hashed password
+                    accountService.updatePassword(accountDetails.getId(), encodedPassword);
+                    infoLogger.info("Account with ID {} password updated from legacy to BCrypt", accountDetails.getId());
+
+                    //Authenticate the user
+                    return new UsernamePasswordAuthenticationToken(accountDetails, accountDetails.getPassword(), accountDetails.getAuthorities());
+                } else {
+//                    If password entered does not equal unhashed password in database
+                    warnLogger.warn("Account with ID {} failed authentication", accountDetails.getId());
+                    throw new BadCredentialsException("Bad credentials");
+                }
+            }
+//            If password is hashed in the database, check it matches password entered
+            else if (BCrypt.checkpw(password, accountDetails.getPassword())){
+                infoLogger.info("Account with ID {} successfully authenticated", accountDetails.getId());
+
+                //Authenticate the user
+                return new UsernamePasswordAuthenticationToken(accountDetails, accountDetails.getPassword(), accountDetails.getAuthorities());
+            } else {
+//                If password does not match hashed password in database
+                warnLogger.warn("Account with ID {} failed authentication", accountDetails.getId());
+                throw new BadCredentialsException("Authentication failed for " + username);
+            }
+        }
+        return null;
     }
 
     @Override
--- src/main/java/com/nsa/evolve/service/impl/AccountDetailsService.java
+++ src/main/java/com/nsa/evolve/service/impl/AccountDetailsService.java
@@ -3,6 +3,8 @@ package com.nsa.evolve.service.impl;
 import com.nsa.evolve.dao.AccountDAO;
 import com.nsa.evolve.dto.Account;
 import com.nsa.evolve.dto.AccountDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,10 +18,15 @@ public class AccountDetailsService implements UserDetailsService {
     @Autowired
     private AccountDAO accountDAO;
 
+    private static final Logger warnLogger = LoggerFactory.getLogger("WarnLog");
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountDAO.findAccountByUsername(username);
-        if (account == null) throw new UsernameNotFoundException("Username Not Found");
+        if (account == null) {
+            warnLogger.warn("Account with username {} was not found when authenticating", username);
+            throw new UsernameNotFoundException("Username Not Found");
+        }
         else return new AccountDetails(account);
     }
 }
